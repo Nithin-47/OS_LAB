f227154d513f1019989350b69b8653cccc9b5585
Nithin-47 <100336015+Nithin-47@users.noreply.github.com>
2023-07-03 13:12:08 +0530

    Create Round-Robin.C

---
+++ Round-Robin.C
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, wt = 0, tat = 0, n, quantum, sum = 0, c = 0;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    int y = n, at[n], bt[n], temp[n];
+    for (i = 0; i < n; i++)
+    {
+        printf("\n Enter the Arrival and Burst time of the Process[%d]: ", i + 1);
+        scanf("%d", &at[i]);
+        scanf("%d", &bt[i]);
+        temp[i] = bt[i];
+    }
+
+    printf("Enter the time quantum for the process: \t");
+    scanf("%d", &quantum);
+
+    printf("\n Process No \t\t Burst Time \t\t TAT \t\t Waiting Time ");
+    for (sum = 0, i = 0; y != 0;)
+    {
+        if (temp[i] <= quantum && temp[i] > 0)
+        {
+            sum = sum + temp[i];
+            temp[i] = 0;
+            c = 1;
+        }
+        else if (temp[i] > 0)
+        {
+            temp[i] = temp[i] - quantum;
+            sum = sum + quantum;
+        }
+
+
+        if (temp[i] == 0 && c == 1)
+        {
+            y--;
+            printf("\nProcess No[%d] \t\t %d\t\t\t %d\t\t\t %d", i + 1, bt[i], sum - at[i], sum - at[i] - bt[i]);
+            wt = wt + sum - at[i] - bt[i];
+            tat = tat + sum - at[i];
+            c = 0;
+        }
+
+
+        if (i == n - 1)
+        {
+            i = 0;
+        }
+        else if (at[i + 1] <= sum)
+        {
+            i++;
+        }
+        else
+        {
+            i = 0;
+        }
+
+    }
+
+    float avg_wt, avg_tat;
+    avg_wt = wt * 1.0 / n;
+    avg_tat = tat * 1.0 / n;
+    printf("\n Average Turn Around Time: \t%f", avg_wt);
+    printf("\n Average Waiting Time: \t%f", avg_tat);
+    return 0;
+}
