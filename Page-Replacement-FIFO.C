88c74e652230c939d5591ba62cf406189152f9e7
Nithin-47 <100336015+Nithin-47@users.noreply.github.com>
2023-08-21 11:56:54 +0530

    Update Page-Replacement-FIFO.C

--- Page-Replacement-FIFO.C
+++ Page-Replacement-FIFO.C
@@ -1,47 +1,49 @@
-#include <stdio.h>  
-int main()  
-{  
-    int incomingStream[] = {4 , 1 , 2 , 4 , 5};  
-    int pageFaults = 0;  
-    int frames = 3;  
-    int m, n, s, pages;   
-    pages = sizeof(incomingStream)/sizeof(incomingStream[0]);   
-    printf(" Incoming \ t Frame 1 \ t Frame 2 \ t Frame 3 ");  
-    int temp[ frames ];  
-    for(m = 0; m < frames; m++)  
-    {  
-        temp[m] = -1;  
-    }  
-    for(m = 0; m < pages; m++)  
-    {  
-        s = 0;   
-        for(n = 0; n < frames; n++)  
-        {  
-            if(incomingStream[m] == temp[n])  
-            {  
-                s++;  
-                pageFaults--;  
-            }  
-        }  
-        pageFaults++;  
-        if((pageFaults <= frames) && (s == 0))  
-        {  
-            temp[m] = incomingStream[m];  
-        }  
-        else if(s == 0)  
-        {  
-            temp[(pageFaults - 1) % frames] = incomingStream[m];  
-        }  
-        printf("\n");  
-        printf("%d\t\t\t",incomingStream[m]);  
-        for(n = 0; n < frames; n++)  
-        {  
-            if(temp[n] != -1)  
-                printf(" %d\t\t\t", temp[n]);  
-            else  
-                printf(" - \t\t\t");  
-        }  
-    }  
-    printf("\nTotal Page Faults:\t%d\n", pageFaults);  
-    return 0;  
-}  
+#include <stdio.h>
+
+#define FRAME_SIZE 3 
+
+
+int findPageInFrames(int frames[], int page, int frameCount) {
+    for (int i = 0; i < frameCount; i++) {
+        if (frames[i] == page) {
+            return 1; 
+        }
+    }
+    return 0; 
+}
+
+int main() {
+    int referenceString[] = {2, 3, 4, 2, 1, 3, 7, 5, 4, 3};
+    int referenceLength = sizeof(referenceString) / sizeof(referenceString[0]);
+    int frames[FRAME_SIZE] = {-1}; 
+    int frameIndex = 0; 
+
+    int pageFaults = 0;
+
+    for (int i = 0; i < referenceLength; i++) {
+        int currentPage = referenceString[i];
+
+        
+        if (!findPageInFrames(frames, currentPage, FRAME_SIZE)) {
+            
+            frames[frameIndex] = currentPage;
+            frameIndex = (frameIndex + 1) % FRAME_SIZE; 
+            pageFaults++;
+        }
+
+        
+        printf("Frames: ");
+        for (int j = 0; j < FRAME_SIZE; j++) {
+            if (frames[j] != -1) {
+                printf("%d ", frames[j]);
+            } else {
+                printf("- ");
+            }
+        }
+        printf("\n");
+    }
+
+    printf("Total Page Faults: %d\n", pageFaults);
+
+    return 0;
+}
