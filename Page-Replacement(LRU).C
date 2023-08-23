d9882b50de0268c602ad25efc308810fbf453cf1
Nithin-47 <100336015+Nithin-47@users.noreply.github.com>
2023-08-23 23:42:31 +0530

    Update Page-Replacement(LRU).C

--- Page-Replacement(LRU).C
+++ Page-Replacement(LRU).C
@@ -1,37 +1,88 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+int findLRU(int time[], int n)
+{
+    int i, minimum = time[0], pos = 0;
+
+    for (i = 1; i < n; ++i)
+    {
+        if (time[i] < minimum)
+        {
+            minimum = time[i];
+            pos = i;
+        }
+    }
+    return pos;
+}
 
 int main(void)
 {
-    int i, n, TotalHeadMoment = 0, initial, count = 0;
-    printf("Enter the number of Requests: ");
-    scanf("%d", &n);
-    int RQ[n];
+    int no_of_frames, no_of_pages, counter = 0, flag1, flag2, i, j, pos, faults = 0;
+    printf("Enter number of frames: ");
+    scanf("%d", &no_of_frames);
+    int frames[no_of_frames];
 
-    printf("Enter the Requests sequence: ");
-    for (i = 0; i < n; i++)
-        scanf("%d", &RQ[i]);
+    printf("Enter number of pages: ");
+    scanf("%d", &no_of_pages);
+    int pages[no_of_pages];
 
-    printf("Enter initial head position: ");
-    scanf("%d", &initial);
-
-    while (count != n)
+    int time[no_of_frames];
+    printf("Enter reference string: ");
+    for (i = 0; i < no_of_pages; ++i)
     {
-        int min = 1000, d, index;
-        for (i = 0; i < n; i++)
-        {
-            d = abs(RQ[i] - initial);
-            if (min > d)
-            {
-                min = d;
-                index = i;
-            }
-        }
-        TotalHeadMoment = TotalHeadMoment + min;
-        initial = RQ[index];
-        RQ[index] = 1000;
-        count++;
+        scanf("%d", &pages[i]);
     }
 
-    printf("Total head movement is: %d", TotalHeadMoment);
+    for (i = 0; i < no_of_frames; ++i)
+    {
+        frames[i] = -1;
+    }
+
+    for (i = 0; i < no_of_pages; ++i)
+    {
+        flag1 = flag2 = 0;
+
+        for (j = 0; j < no_of_frames; ++j)
+        {
+            if (frames[j] == pages[i])
+            {
+                counter++;
+                time[j] = counter;
+                flag1 = flag2 = 1;
+                break;
+            }
+        }
+
+        if (flag1 == 0)
+        {
+            for (j = 0; j < no_of_frames; ++j)
+            {
+                if (frames[j] == -1)
+                {
+                    counter++;
+                    faults++;
+                    frames[j] = pages[i];
+                    time[j] = counter;
+                    flag2 = 1;
+                    break;
+                }
+            }
+        }
+
+        if (flag2 == 0)
+        {
+            pos = findLRU(time, no_of_frames);
+            counter++;
+            faults++;
+            frames[pos] = pages[i];
+            time[pos] = counter;
+        }
+        printf("\n");
+
+        for (j = 0; j < no_of_frames; ++j)
+        {
+            printf("%d\t", frames[j]);
+        }
+    }
+    printf("\n\nTotal Page Faults = %d", faults);
 }
